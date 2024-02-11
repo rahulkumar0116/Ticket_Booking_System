4e5b3d523343008fb3ab7e1a7d6cc0b0b02d1453
Rahul <rahulkumar0116@gmail.com>
2024-02-11 08:35:46 +0530

    ticket controller Updated

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -7,49 +7,6 @@ import { User } from "../models/user.js";
 import { ApiResponce } from "../utils/ApiResponce.js";
 import { Stop } from "../models/stop.js";
 
-const createTicket = asyncHandler(async (req, res) => {
-  const { start, stop, date, seatNo } = req.body;
-  if (!start && !stop && !date) {
-    throw new ApiError(404, "All fileds are required ");
-  }
-  const Source = await Stop.findOne({ code: start });
-  const Destination = await Stop.findOne({ code: stop });
-  const foundTrip = await Trip.findOne({
-    sourceStop: Source._id,
-    desStop: Destination._id,
-  });
-  if (!foundTrip) {
-    throw new ApiError(404, "Trip not found");
-  }
-  const foundTripSchedule = await TripSchedule.findOne({
-    tripDetails: foundTrip._id,
-    tripDate: date,
-  }).populate({
-    path: "tripDetails",
-  });
-  if (!foundTripSchedule) {
-    throw new ApiError(404, "No Trip schedule is found");
-  }
-  const user = await User.findById(req.user._id).select(
-    "-password -email -username -role -__v -refreshToken "
-  );
-  if (!user) {
-    throw new ApiError(500, "user not found");
-  }
-  const ticket = await Ticket.create({
-    seatNumber: seatNo,
-    cancellable: true,
-    journeyDate: date,
-    passenger: user,
-    tripSchedule: foundTripSchedule,
-  });
-  if (!ticket) {
-    throw new ApiError(404, "Ticket not found");
-  }
-  return res
-    .status(200)
-    .json(new ApiResponce(200, ticket, "Ticket created successfully"));
-});
 
 const bookTicket = asyncHandler(async (req, res) => {
   const { start, stop, date, seatNo } = req.body;
@@ -100,4 +57,4 @@ const bookTicket = asyncHandler(async (req, res) => {
   
 });
 
-export { createTicket, bookTicket };
+export { bookTicket };
